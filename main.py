6221258e815aa5284f798c60f0ec7de00d6132d4
christopherl112 <lopezchristopher909@gmail.com>
2025-08-20 12:28:38 -0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import math
-import pandas
-import seaborn
-import matplotlib.pyplot as plt
-
-international_teams = pandas.read_csv("../datasets/international_matches.csv")
-
-international_teams.head(10)
